3198c0fe67c6413a8f70e1998360e2d55d8d76fc
Gehan Pasindhu <54250592+GehanPasindhu@users.noreply.github.com>
2022-03-13 19:58:29 +0530

    Create createcsv.php

---
+++ createcsv.php
@@ -0,0 +1,8 @@
+<?php
+
+header("Content-type:text/plain");
+header("Content-Disposition: attachment; filename=yourfilename.csv");
+
+echo '"A","B","C","D"'; /* Sample data set for csv file */
+
+?>
